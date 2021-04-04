46eb11d57b3b59ae7b7782b7615dbca4b68b9594
mkbaraka <mikel.martinezz@opendeusto.es>
2021-04-04 12:06:35 +0200

    cliente.h y cliente.c creadas

---
+++ Modulo C/Cliente.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Cliente.h"
+
+void void imprimirCliente(Cliente c)
+{
+    printf("Dni: %s,Nombre: %s, Apellido: %s, cp: %i, Contraseña: %s,"
+    " Fecha de nacimiento: %i\n ", c.dni, c.nombre, c.apellido, c.cp, c.contra, c.fNac);
+}
---
+++ Modulo C/Cliente.h
@@ -0,0 +1,17 @@
+#ifndef CLIENTE_H_
+#define CLIENTE_H_
+
+
+typedef struct {
+        char* dni;
+        char* nombre;
+        char* apellido;
+        int* cp;
+        char* contra;
+        int fNac ;
+
+} Cliente;
+
+void imprimirCliente(Cliente c);
+
+#endif /* CLIENTE_H_ */
