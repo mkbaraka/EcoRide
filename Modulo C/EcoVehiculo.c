861aed6ba1d2089e090d0e1c62601222b8b67e7e
mkbaraka <mikel.martinezz@opendeusto.es>
2021-04-05 10:34:24 +0200

    metodo seleccionar ecovehiculo

--- Modulo C/EcoVehiculo.c
+++ Modulo C/EcoVehiculo.c
@@ -4,7 +4,6 @@
 
 
 
-
 void visualizarEcoVehiculos(EcoVehiculo e){
 
 	printf("Nombre --> %s\n", e.nombre);
@@ -21,73 +20,55 @@ int seleccionarEcoVehiculo(){
 	int sel;
 
 
-	printf("Seleccione un EcoVehiculo/n");
-	printf("Patinetes electricas\n");
-	printf("1. Patinete Electrice Xiaomi 15\n" );
-	printf("2. Patinete Electrico Ninebot Segway Max 630\n" );
-	printf("3. Patinete Electrico CECOTEC Outsider Demigod Makalu \n");
-	printf("4. Patinete Electrico Ninebot KickScooter E45E\n");
-	printf("5. Patinete Electrico EGRET-TEN V3 x 48V\n");
-	printf("\n");
-	printf("Bicicletas electricas\n" );
-	printf("6. Bicicleta Electrica de carretera Orbea Gain 030 1X\n");
-	printf("7. Mountain Bike Electrica Orbea Wild F5 H30 \n");
-	printf("8. Specialized bicicleta completa Turbo Vado 51 5.0 \n");
-	printf("9. Bicicleta emtb lapierre overvolt am 5.5 \n");
-	printf("10. Bicicleta Cannondale Moterra \n");
 
-	fflush(stdin);
-	scanf("%i",&sel);
+	printf("Seleccione un EcoVehiculo\n");
+		printf("Patinetes electricas\n");
+		printf("1. Patinete Electrice Xiaomi 15\n" );
+		printf("2. Patinete Electrico Ninebot Segway Max 630\n" );
+		printf("3. Patinete Electrico CECOTEC Outsider Demigod Makalu \n");
+		printf("4. Patinete Electrico Ninebot KickScooter E45E\n");
+		printf("5. Patinete Electrico EGRET-TEN V3 x 48V\n");
+		printf("\n");
+		printf("Bicicletas electricas\n" );
+		printf("6. Bicicleta Electrica de carretera Orbea Gain 030 1X\n");
+		printf("7. Mountain Bike Electrica Orbea Wild F5 H30 \n");
+		printf("8. Specialized bicicleta completa Turbo Vado 51 5.0 \n");
+		printf("9. Bicicleta emtb lapierre overvolt am 5.5 \n");
+		printf("10. Bicicleta Cannondale Moterra \n");
 
-// creo que esta mal este for
-	int Eco = 0;
+		fflush(stdin);
+		printf("Introduce un numero entre el 1-10\n");
+		scanf("%i",&sel);
+		printf("\n\n");
 
-	for(i=0; i< strlen(sel); i++){
 
-	if(sel[i]=="1"){
-		eco = 1;
-	}if(sel[i]=="2"){
-		eco = 2;
-	}if(sel[i]=="3"){
-		eco = 3;
-	}if(sel[i]=="4"){
-		eco = 4;
-	}if(sel[i]=="5"){
-		eco = 5;
-	}if(sel[i]=="6"){
-		eco = 6;
-	}if(sel[i]=="7"){
-		eco = 7;
-	}if(sel[i]=="8"){
-		eco = 8;
-	}if(sel[i]=="9"){
-		eco = 9;
-	}if(sel[i]=="10"){
-		eco = 10;
-	}else{
-		printf("Seleccione un numero correcto\n");
+
+
+		 if (sel == 1){
+			 printf("Has seleccionado Patinete Electrice Xiaomi 15\n" );
+		 }else if(sel == 2){
+			 printf("Has seleccionado Patinete Electrico Ninebot Segway Max 630\n" );
+
+		 }else if(sel == 3){
+			 printf("Has seleccionado Patinete Electrico CECOTEC Outsider Demigod Makalu \n");
+		 }else if(sel == 4){
+			 printf("Has seleccionado Patinete Electrico Ninebot KickScooter E45E\n");
+		 }else if(sel == 5){
+			 printf("Has seleccionado Patinete Electrico EGRET-TEN V3 x 48V\n");
+		 }else if(sel == 6){
+			  printf("Has seleccionado Bicicleta Electrica de carretera Orbea Gain 030 1X\n");
+		 }else if(sel == 7){
+			 printf("Has seleccionado Mountain Bike Electrica Orbea Wild F5 H30 \n");
+		 }else if(sel == 8){
+			 printf("Has seleccionado Specialized bicicleta completa Turbo Vado 51 5.0 \n");
+		 }else if(sel == 9){
+			 printf("Has seleccionado Bicicleta emtb lapierre overvolt am 5.5 \n");
+		 }else if(sel == 10){
+			 printf("Has seleccionado Bicicleta Cannondale Moterra \n");
+		}else{
+			printf("Seleccione un numero correcto\n");
+		}
+
+		return sel;
 	}
-}
-	return sel;
-
-}
-
-void modificarDisponibilidad(Vehiculo *v){
-
-	visualizarEcoVehiculos(*v);
-	int d;
-
-	printf("Escriba la nueva disponibilidad \n");
-	fflush(stdin);
-	scanf("%i", &d);
-
-	if(d>=0){
-		v-> disponibles = d;
-	}else{
-		printf("En negativo no esta admitido la disponibilidad\n");
-	}
-
-
-	return;
-}
 
