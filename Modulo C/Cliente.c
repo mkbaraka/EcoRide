dd398ba7d98076cacbd6bd98d0735f0a3d250be1
mkbaraka <mikel.martinezz@opendeusto.es>
2021-04-09 12:21:40 +0200

    quitar void

--- Modulo C/Cliente.c
+++ Modulo C/Cliente.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "Cliente.h"
 
-void void imprimirCliente(Cliente c)
+void imprimirCliente(Cliente c)
 {
     printf("Dni: %s,Nombre: %s, Apellido: %s, cp: %i, Contrasena: %s,"
     " Fecha de nacimiento: %i\n ", c.dni, c.nombre, c.apellido, c.cp, c.contra, c.fNac);
